63c7b732b7d91ff8fe1ad5bf44df69eddc617865
Thu <hyatmyat779@gmail.com>
2023-12-02 20:19:02 +0630

    add: index.html

--- js/script.js
+++ js/script.js
@@ -3,3 +3,9 @@ const search = document.querySelector(".searchInput");
 document.querySelector(".searchIcon").onclick = () => {
   search.classList.toggle("active");
 };
+
+const menu = document.querySelector(".menuIconp");
+
+document.querySelector(".menuIcon").onclick = () => {
+  menu.classList.toggle("activeA");
+};
